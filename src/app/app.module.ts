65b0b5562f7318abbe6e21545f1c458f542f66b6
juliokasahara <juliokasahara@gmail.com>
2020-01-25 11:53:47 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.componet';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponet,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
