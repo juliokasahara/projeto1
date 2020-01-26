1768393428f4eed479548c7927ddb15e91f981f0
juliokasahara <juliokasahara@gmail.com>
2020-01-26 11:33:57 -0300

    aula 1 ate 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.componet';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponet,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
